4a8ef17765129cef0d040a8cc7f44120ea22b9ee
Robson S. Rodrigues <robsoncraftsman@gmail.com>
2020-07-17 17:00:43 -0300

    Ajustado erro de ortografia na varável postgresStrategy

--- 06-postgres/src/database/strategies/PostgresStrategy.test.js
+++ 06-postgres/src/database/strategies/PostgresStrategy.test.js
@@ -1,7 +1,7 @@
 const { equal, deepEqual, ok } = require("assert");
 
 const PostgresStrategy = require("./PostgresStrategy");
-const postgressStrategy = new PostgresStrategy();
+const postgresStrategy = new PostgresStrategy();
 
 const EXISTING_HERO = { id: 1, nome: "Flash", poder: "Velocidade" };
 const NEW_HERO = { id: 2, nome: "IronMan", poder: "Tech" };
@@ -12,51 +12,51 @@ describe("PostgresStrategy", function () {
   this.timeout(Infinity);
 
   this.beforeAll(async function () {
-    await postgressStrategy.clear();
-    await postgressStrategy.create(EXISTING_HERO);
+    await postgresStrategy.clear();
+    await postgresStrategy.create(EXISTING_HERO);
   });
 
   it("Init connection", async function () {
-    const connected = await postgressStrategy._connect();
+    const connected = await postgresStrategy._connect();
     ok(connected);
   });
 
   it("#create", async function () {
-    const insertedHero = await postgressStrategy.create(NEW_HERO);
+    const insertedHero = await postgresStrategy.create(NEW_HERO);
     deepEqual(NEW_HERO, insertedHero);
   });
 
   it("#read", async function () {
-    const heroes = await postgressStrategy.read(EXISTING_HERO.id);
+    const heroes = await postgresStrategy.read(EXISTING_HERO.id);
     ok(heroes.length === 1);
     const readedHero = heroes[0];
     deepEqual(EXISTING_HERO, readedHero);
   });
 
   it("#update", async function () {
-    await postgressStrategy.create(UPDATE_HERO);
-    const [updateCount] = await postgressStrategy.update(
+    await postgresStrategy.create(UPDATE_HERO);
+    const [updateCount] = await postgresStrategy.update(
       UPDATE_HERO.id,
       NEW_POWER
     );
     equal(updateCount, 1);
-    const [updatedHero] = await postgressStrategy.read(UPDATE_HERO.id);
+    const [updatedHero] = await postgresStrategy.read(UPDATE_HERO.id);
     const expectedHero = { ...UPDATE_HERO, ...NEW_POWER };
     deepEqual(expectedHero, updatedHero);
   });
 
   it("#delete", async function () {
-    const deleteCount = await postgressStrategy.delete(EXISTING_HERO.id);
+    const deleteCount = await postgresStrategy.delete(EXISTING_HERO.id);
     equal(deleteCount, 1);
   });
 
   it("#clear", async function () {
-    const deleteCount = await postgressStrategy.clear();
+    const deleteCount = await postgresStrategy.clear();
     ok(deleteCount > 0);
   });
 
   it("Close connection", async function () {
-    const closed = await postgressStrategy._close();
+    const closed = await postgresStrategy._close();
     ok(closed);
   });
 });
